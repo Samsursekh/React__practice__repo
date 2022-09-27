35beff7eb41f8dc8b4f8b3a5d897d62ff2548c15
Samsursekh <sekhsamsur44@gmail.com>
2022-09-27 08:02:52 +0530

    useEffect__practice__added

--- react__practice/src/Components/Posts.jsx
+++ react__practice/src/Components/Posts.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 
-function getData(){
-   return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5`).then(
+function getData({page}){
+   return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`).then(
     res=>{
        return res.json();
     })
@@ -14,28 +15,51 @@ function getData(){
 
 
 const Posts = () => {
+const [loading,setLoading] = useState(false)
+const [page,setPage] = useState(1);
+
+  useEffect(()=>{
+    handleFetchData(page);
+  },[page]);
+
 const [posts,setPosts] = useState([])
-const handleFetchData = async()=> {
+const handleFetchData = async(page=1)=> {
     try{
-       const data = await getData();
+        setLoading(true);
+       const data = await getData({page});
        setPosts(data);
-       console.log(data);
+       setLoading(false)
+      //  console.log(data);
     }
     catch(err){
+      setLoading(false);
         console.log("error");
     }
 }    
 console.log(posts);
+
+const handlePageChange =(changeBy) => {
+  setPage(page+changeBy);
+  // handleFetchData(page+changeBy);    or i can pass page in the 
+  // dependancy array itself
+}
+
+if(loading){
+  return <h3>Loading</h3>
+}
   return (
     <div>
       <h3>Posts</h3>
-      
-      <button onClick={handleFetchData}>GET POSTS</button>
+        {/* {loading && <h3>Loading</h3>}      */}
+      {/* <button onClick={handleFetchData}>GET POSTS</button> */}
      <ul>
         {
-            posts.map(item=> <li key={item.id}>{item.title}</li>)
+            posts.map(item=> <li id={item.id} key={item.id}>  {item.title}</li>)
         }
      </ul>
+     <button onClick={()=>handlePageChange(-1) } disabled={page === 1}>PREV</button>
+     <button>{page}</button>
+     <button onClick={()=> handlePageChange(+1)} disabled={page === 10}>NEXT</button>
     </div>
   )
 }
